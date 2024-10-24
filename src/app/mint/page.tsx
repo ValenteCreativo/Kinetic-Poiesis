dc367f5731b82cdf1e0e30b75e76c35fb28f56cb
Valentín Martínez <valentecreativo@proton.me>
2024-10-24 05:41:59 -0600

    Mint page created for deploy testing

---
+++ src/app/mint/page.tsx
@@ -0,0 +1,13 @@
+// src/app/mint/page.tsx
+import React from 'react';
+
+const MintPage = () => {
+  return (
+    <div>
+      <h1>Mint your NFT</h1>
+      <p>This page will allow users to mint NFTs.</p>
+    </div>
+  );
+};
+
+export default MintPage;
