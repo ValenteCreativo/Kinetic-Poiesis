77861c17c1b3eb70d3999564192f910b715c7bdc
Valentín Martínez <valentecreativo@proton.me>
2024-10-24 14:29:17 -0600

    Next problem solved hope we can deploy now

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['coral-near-warbler-359.mypinata.cloud'], // Add the domain you want to load images from
+  },
+};
+
+module.exports = nextConfig;
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['coral-near-warbler-359.mypinata.cloud'], // Agrega aquí el dominio del cual quieres cargar imágenes
-  },
-};
-
-export default nextConfig;
